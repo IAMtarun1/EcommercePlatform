ade10a08d2dd687eb0103910186f3368fa976034
IAMtarun1 <tarunsinghrajput11@gmail.com>
2026-03-31 14:28:49 -0400

    Adding frontend

--- src/test/java/com/ecommerce/ecommerceplatform/EcommercePlatformApplicationTests.java
+++ src/test/java/com/ecommerce/ecommerceplatform/EcommercePlatformApplicationTests.java
@@ -2,12 +2,14 @@ package com.ecommerce.ecommerceplatform;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EcommercePlatformApplicationTests {
 
     @Test
     void contextLoads() {
+        // Test that the application context loads successfully
     }
-
 }
